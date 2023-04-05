a395c8aa2fb9c11c3365127720f0e71a98d85190
p-jura <themadman@o2.pl>
2023-04-05 22:19:01 +0200

    createing draft of application

---
+++ lib/view_pdf/presentation/pdf_view_widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+
+class PdfViewWidget extends StatefulWidget {
+  const PdfViewWidget({
+    super.key,
+  });
+  static String route = '/pdf_view';
+  @override
+  State<PdfViewWidget> createState() => _PdfViewWidgetState();
+}
+
+class _PdfViewWidgetState extends State<PdfViewWidget> {
+  final _title = 'View PDF';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_title),
+      ),
+      body: SfPdfViewer.asset(
+        'assets/example.pdf',
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Load pdf',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
